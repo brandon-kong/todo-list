d7553e414af61f447a239c2551064a341ffbf353
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2022-04-30 12:34:34 -0400

    Included templates

--- list/views.py
+++ list/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 from .models import User, Item
 # Create your views here.
 def index(request):
     return render(request, 'list/index.html', {
-        'list': Item.objects.order_by('time_posted')[:5]
+        'list': Item.objects.order_by('time_posted')
     })
 
 def create(request):
-    return HttpResponse()
+    if request.method == "POST":
+        data = request.POST
+        if (len(data.get('text')) < 1):
+            return render(request, 'list/create.html', {
+            'list': Item.objects.order_by('time_posted'),
+            'error': 'Your text must have 1+ characters'
+        })
+        else:
+            toSave = Item(text=data.get('text'), author=request.user)
+            toSave.save()
+    return render(request, 'list/create.html', {
+        'list': Item.objects.order_by('time_posted')
+    })
 
 def view(request, id):
     items = Item.objects.order_by('time_posted')[:5]
